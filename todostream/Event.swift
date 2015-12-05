09b972f9d66e89d9aa59799b1b36cfc6f34381e6
Christopher Trott <chris@twocentstudios.com>
2015-12-05 18:47:38 +0900

    Initial PersistenceController & Events

--- todostream/Event.swift
+++ todostream/Event.swift
@@ -3,6 +3,13 @@
 //  Copyright © 2015 twocentstudios. All rights reserved.
 //
 
+import Foundation
+import Result
+
 enum Event {
+    case ReqReadTodos
+    case ReqWriteTodo(Todo)
     
+    case ResTodos(Result<[Todo], NSError>)
+    case ResTodo(Result<Todo, NSError>)
 }
---
+++ todostream/PersistenceController.swift
@@ -0,0 +1,96 @@
+//
+//  Created by Christopher Trott on 12/5/15.
+//  Copyright © 2015 twocentstudios. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import ReactiveCocoa
+import Result
+
+protocol RealmEncodable {
+    typealias RealmObjectType: Object
+    var realmObject: RealmObjectType { get }
+}
+
+protocol RealmDecodable {
+    typealias DomainObjectType
+    var domainObject: DomainObjectType { get }
+}
+
+enum PersistenceError: ErrorType {
+    case UnknownError
+    case RealmError(NSError)
+}
+
+struct PersistenceController {
+    let configuration: Realm.Configuration
+    
+    var database: Result<Realm, PersistenceError> {
+        return Realm.result(configuration)
+    }
+    
+    init(configuration: Realm.Configuration, appContext: AppContext) {
+        self.configuration = configuration
+        
+        /// .ReqReadTodos
+        appContext.eventsSignal
+            .filter {
+                switch $0 {
+                case .ReqReadTodos: return true
+                default: return false
+                }
+            }
+            .map { _ in self.database.map { $0.objects(TodoObject).decodeResults() }.mapError { _ in NSError.app() } }
+            .map { Event.ResTodos($0) }
+            .observe(appContext.eventsObserver)
+        
+        /// .ReqWriteTodo
+        appContext.eventsSignal
+            .map { event -> Todo? in
+                switch event {
+                case .ReqWriteTodo(let todo): return todo
+                default: return nil
+                }
+            }
+            .ignoreNil()
+            .map { $0.realmObject }
+            .map { todoObject in
+                return self.database
+                    .flatMap { realm in
+                        realm.writeObject(todoObject).map { $0.domainObject }
+                    }
+                    .mapError { _ in NSError.app() }
+            }
+            .map { Event.ResTodo($0) }
+            .observe(appContext.eventsObserver)
+    }
+}
+
+extension Realm {
+    static func result(configuration: Configuration) -> Result<Realm, PersistenceError> {
+        do {
+            let realm = try Realm(configuration: configuration)
+            return Result(value: realm)
+        } catch let error as NSError {
+            return Result(error: .RealmError(error))
+        }
+    }
+    
+    func writeObject<T: Object>(object: T) -> Result<T, PersistenceError> {
+        do {
+            try self.write {
+                self.add(object, update: true)
+            }
+            return Result(value: object)
+        } catch let error as NSError {
+            return Result(error: .RealmError(error))
+        }
+    }
+}
+
+extension Results where T: RealmDecodable {
+    func decodeResults() -> [T.DomainObjectType] {
+        return Array(self).map { obj -> T.DomainObjectType in obj.domainObject }
+    }
+}
